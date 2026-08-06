3d6e40c37e4a5e58d12614a7fa4f0b8d648705c2
suleiman <suleimanosman1023@gmail.com>
2026-08-06 11:44:28 +0300

    started arrays

---
+++ week9/Arrays.java
@@ -0,0 +1,37 @@
+public class Arrays {
+    public static void main(String[] args){
+        // int[] numbers = {1, 3, 5, 7, 9};
+        // int numbers[] = {1, 3, 5, 7, 9};
+        // System.out.println(numbers[3]);
+
+        // String[] cars = new String[4]; // size is 4
+        // cars[0] = "Volvo";
+        // cars[1] = "BMW";
+        // cars[2] = "Ford";
+        // cars[3] = "Mazda";
+        // // System.out.println(cars[0]); // Outputs Volvo
+
+        // for (int i = 0; i < cars.length; i++) {
+        //     System.out.println(cars[i]);
+        // }
+
+        // Calculate the Sum of Elements
+        // int[] numbers = {1, 5, 10, 25};
+        // int sum = 0;
+        // for (int i = 0; i < numbers.length; i++) {
+        //     sum = sum + numbers[i];
+        // }
+        // System.out.println("The sum is: " + sum);
+
+        // // for each syntax
+        // for (type variable : arrayname) {
+        // // code block to be executed
+        // }
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String car : cars) {
+            System.out.println(car);
+        }
+
+    }
+}
